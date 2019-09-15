737e85a45917c1994602f85cfa206bee08aef9ee
JK <38530893+JaraKucera@users.noreply.github.com>
2019-09-15 12:42:08 +0100

    Update#2

---
+++ Assets/Scripts/GemSilver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSilver : MonoBehaviour
+{
+    
+    public GameObject ScoreBox;
+
+    void OnTriggerEnter() {
+        Destroy(gameObject);
+    }
+
+
+}
--- Assets/unity-chan!/Unity-chan! Model/Scripts/FaceUpdate.cs
+++ Assets/unity-chan!/Unity-chan! Model/Scripts/FaceUpdate.cs
@@ -15,19 +15,6 @@ namespace UnityChan
 			anim = GetComponent<Animator> ();
 		}
 
-		void OnGUI ()
-		{
-			GUILayout.Box ("Face Update", GUILayout.Width (170), GUILayout.Height (25 * (animations.Length + 2)));
-			Rect screenRect = new Rect (10, 25, 150, 25 * (animations.Length + 1));
-			GUILayout.BeginArea (screenRect);
-			foreach (var animation in animations) {
-				if (GUILayout.RepeatButton (animation.name)) {
-					anim.CrossFade (animation.name, 0);
-				}
-			}
-			isKeepFace = GUILayout.Toggle (isKeepFace, " Keep Face");
-			GUILayout.EndArea ();
-		}
 
 		float current = 0;
 
